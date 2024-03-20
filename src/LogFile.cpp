8719d3a6ac59c7a4b0ea0a02d1e316d85a5afbac
Fenrir7Asteron <b.fedotov@everplay.dev>
2024-03-20 18:29:09 +0300

    Create separate log files for NRC and MC renderers, add loss to NRC logging

--- src/LogFile.cpp
+++ src/LogFile.cpp
@@ -4,24 +4,36 @@
 
 namespace en
 {
-	LogFile::LogFile(const std::string& filePath)
+	LogFile::LogFile(const std::string& filePath) : m_FilePath(filePath)
 	{
 		if (std::filesystem::exists(filePath))
 		{
 			Log::Info("Log file (" + filePath + ") already exists -> deleting it");
 			std::filesystem::remove(filePath);
 		}
-
-		m_File = std::ofstream(filePath);
 	}
 
 	LogFile::~LogFile()
 	{
-		m_File.close();
+		if (m_File.is_open())
+		{
+			m_File.close();
+		}
+	}
+
+	void LogFile::OpenLogFile()
+	{
+		Log::Info("Open file (" + m_FilePath + ")");
+		m_File = std::ofstream(m_FilePath);
 	}
 
 	void LogFile::WriteLine(const std::string& line)
 	{
+		if (!m_File.is_open())
+		{
+			OpenLogFile();
+		}
+
 		m_File << line << std::endl;
 	}
 }
