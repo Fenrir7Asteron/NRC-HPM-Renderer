3e2c28677cb6d1e5b7628b4d05dc1068b94442b1
Fenrir7Asteron <b.fedotov@everplay.dev>
2024-04-08 18:57:00 +0300

    Add biased and unbiased versions of transmittance estimators from the "An unbiased ray-marching transmittance estimator" paper

--- data/shader/include/path_trace.glsl
+++ data/shader/include/path_trace.glsl
@@ -1,3 +1,5 @@
+const int MAX_UNBIASED_POWER_ESTIMATE_COUNT = 8;
+
 float GetTransmittance(const vec3 start, const vec3 end, const uint count)
 {
 	const vec3 dir = end - start;
@@ -42,6 +44,177 @@ float RatioTrack(const vec3 start, const vec3 end)
 	return transmittance;
 }
 
+float GetNForCMF(float controlOpticalThickness)
+{
+	return ceil(pow((0.015 + controlOpticalThickness) * (0.65 + controlOpticalThickness) * (60.3 + controlOpticalThickness), 1.0 / 3.0));
+}
+
+float BKExpectedEvalOrder(float c, float pz)
+{
+	float K = floor(c);
+	float t = 1.0;
+	float sum = 1.0;
+
+	for (int k = 1; k <= K; ++k)
+	{
+		t = t * c / float(k);
+		sum += t;
+	}
+
+	float En = K + (exp(c) - sum) / t;
+
+	// Non-zero orders are evaluated with probability 1 - pz
+	return (1 - pz) * En;
+}
+
+int DetermineTupleSize(float controlOpticalThickness, float pz)
+{
+	const float NForCMF = GetNForCMF(controlOpticalThickness);
+	const float NForBK = BKExpectedEvalOrder(2.0, pz); // 0.31945
+	return max(1, int(NForCMF / (NForBK + 1.0) + 0.5));
+}
+
+float[MAX_UNBIASED_POWER_ESTIMATE_COUNT] ElementaryMeans(float X[MAX_UNBIASED_POWER_ESTIMATE_COUNT], int N, int Z)
+{
+	float m[MAX_UNBIASED_POWER_ESTIMATE_COUNT];
+	m[0] = 1.0;
+	for (int k = 1; k <= Z; ++k)
+	{
+		m[k] = 0.0;
+	}
+
+	for (int n = 1; n <= N; ++n)
+	{
+		for (int k = min(n, Z); k >= 1; --k)
+		{
+			m[k] = m[k] + (float(k) / float(n)) * (m[k - 1] * X[n] - m[k]);
+		}
+	}
+
+	return m;
+}
+
+int AggressiveBKRoulette(float pz, out float weights[MAX_UNBIASED_POWER_ESTIMATE_COUNT])
+{
+	float p = 1.0 - pz;
+	weights[0] = 1.0;
+	float u = RandFloat(1.0);
+
+	// Stop at the zeroth order term with probability pz
+	if (p <= u)
+		return 0;
+
+	// BK with K = c = 2
+	int K = 2;
+	float c = 2;
+
+	for (int i = 1; i <= K; ++i)
+	{
+		weights[i] = 1.0 / p;
+	}
+
+	int i = K + 1;
+	while (true)
+	{
+		// Compute the continuation probabilities
+		float ci = min(c / float(i), 1.0);
+		// Update the probability of sampling at least order i
+		p *= ci;
+		// Russian roulette termination
+		if (p <= u)
+		{
+			return i - 1;
+		}
+
+		// Final weight for order i
+		weights[i] = 1.0 / p;
+
+		i += 1;
+	}
+}
+
+float UnbiasedRaymarchingTransmittanceEstimator(const vec3 start, const vec3 end, float controlOpticalThickness)
+{
+	const vec3 dir = normalize(end - start);
+	float intervalLength = distance(end, start);
+	const int M = min(DetermineTupleSize(controlOpticalThickness, 0.9), MAX_UNBIASED_POWER_ESTIMATE_COUNT - 2);
+
+	float weights[MAX_UNBIASED_POWER_ESTIMATE_COUNT];
+	const int N = min(AggressiveBKRoulette(0.9, weights), MAX_UNBIASED_POWER_ESTIMATE_COUNT - 2);
+
+	const float d1 = getDensity(start);
+	const float d2 = getDensity(start + dir * intervalLength);
+	const float sampleLength = (intervalLength / float(M));
+	float X[MAX_UNBIASED_POWER_ESTIMATE_COUNT];
+
+	for (int i = 1; i <= N + 1; ++i)
+	{
+		const float u = RandFloat(1.0);
+
+		X[i] = 0.0;
+
+		for (int j = 0; j <= M - 1; j++)
+		{
+			X[i] += getDensity(start + dir * (sampleLength * (u + float(j))));
+		}
+
+		X[i] = -sampleLength * X[i];
+		//X[i] = X[i] - sampleLength * (0.5 - u) * (d2 - d1); // endpoint matching, optional
+	}
+
+	float T = 0;
+	for (int j = 1; j <= N + 1; ++j)
+	{
+		float X_remaining[MAX_UNBIASED_POWER_ESTIMATE_COUNT];
+		float pivot = X[j];
+		for (int k = 1; k < j; ++k)
+		{
+			X_remaining[k] = X[k] - pivot;
+		}
+		
+		// Remove j-th element from X with offsetting array elements one step left.
+		for (int k = j; k <= N; ++k)
+		{
+			X_remaining[k] = X[k + 1] - pivot;
+		}
+
+		float m[MAX_UNBIASED_POWER_ESTIMATE_COUNT] = ElementaryMeans(X_remaining, N, N);
+		float temp = 0.0;
+		float kFactorial = 1;
+		for (int k = 0; k <= N; ++k)
+		{
+			kFactorial *= float(max(1, k));
+			temp += m[k] / (kFactorial * weights[k]);
+		}
+		T = T + (1.0 / float(N + 1)) * exp(pivot) * temp;
+	}
+
+	return T;
+}
+
+
+float BiasedRaymarchingTransmittanceEstimator(const vec3 start, const vec3 end, float controlOpticalThickness)
+{
+	const vec3 dir = normalize(end - start);
+	float intervalLength = distance(end, start);
+	const float M = GetNForCMF(controlOpticalThickness);
+	const float u = RandFloat(1.0);
+	const float d1 = getDensity(start);
+	const float d2 = getDensity(start + dir * intervalLength);
+	const float sampleLength = (intervalLength / M);
+
+	float X = 0.0;
+
+	for (uint i = 0; i < M; i++)
+	{
+		X += getDensity(start + dir * (sampleLength * (u + i)));
+	}
+
+	X = -sampleLength * X;
+	//X = X - sampleLength * (0.5 - u) * (d2 - d1); // endpoint matching, optional
+	return exp(X);
+}
+
 vec3 TraceDirLight(const vec3 pos, const vec3 dir)
 {
 	if (dir_light.strength == 0.0)
@@ -49,7 +222,7 @@ vec3 TraceDirLight(const vec3 pos, const vec3 dir)
 		return vec3(0.0);
 	}
 
-	const float transmittance = RatioTrack(pos, find_entry_exit(pos, -normalize(dir_light.dir))[1]);
+	const float transmittance = BiasedRaymarchingTransmittanceEstimator(pos, find_entry_exit(pos, -normalize(dir_light.dir))[1], VOLUME_DENSITY_FACTOR);
 	const float phase = hg_phase_func(dot(dir_light.dir, -dir));
 	const vec3 dirLighting = vec3(1.0f) * transmittance * dir_light.strength * phase;
 	return dirLighting;
@@ -62,7 +235,7 @@ vec3 TracePointLight(const vec3 pos, const vec3 dir)
 		return vec3(0.0);
 	}
 
-	const float transmittance = RatioTrack(pointLight.pos, pos);
+	const float transmittance = BiasedRaymarchingTransmittanceEstimator(pointLight.pos, pos, VOLUME_DENSITY_FACTOR);
 	const float phase = hg_phase_func(dot(normalize(pointLight.pos - pos), -dir));
 	const vec3 pointLighting = pointLight.color * pointLight.strength * transmittance * phase;
 	return pointLighting;
@@ -100,7 +273,7 @@ vec3 SampleHdrEnvMap(const vec3 pos, const vec3 dir, uint sampleCount)
 		const float phase = hg_phase_func(dot(randomDir, -dir));
 		const vec3 exit = find_entry_exit(pos, randomDir)[1];
 		//const float transmittance = GetTransmittance(pos, exit, 16);
-		const float transmittance = RatioTrack(pos, exit);
+		const float transmittance = BiasedRaymarchingTransmittanceEstimator(pos, exit, VOLUME_DENSITY_FACTOR);
 		const vec3 sampleLight = SampleHdrEnvMap(randomDir) * phase * transmittance;
 
 		light += sampleLight;
