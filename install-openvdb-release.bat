a27f714b300752c03e2c936cc4936a06003f8ba7
Bogdan Fedotov <b.fedotov@everplay.dev>
2024-05-17 23:48:56 +0300

    Install packages with vcpkg instead of using local installations. Update CMakeLists.txt to copy vcpkg packages DLLs and data folders.

---
+++ install-openvdb-release.bat
@@ -0,0 +1,6 @@
+cd openvdb
+if not exist build mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX:PATH="../../openvdb-install" -A x64 ..
+cmake --build . --parallel 16 --config Release --target install
+cd ../..
--- install-openvdb.bat
+++
@@ -1,6 +0,0 @@
-cd openvdb
-if not exist build mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH="../../openvdb-install" -A x64 ..
-cmake --build . --parallel 16 --config Release --target install
-cd ../..
